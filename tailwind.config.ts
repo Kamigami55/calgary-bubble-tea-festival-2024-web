2b4aa13d50c7f45d6ed08f89d1964071ad0c278c
Eason Chang <eason@easonchang.com>
2024-05-05 23:53:47 -0600

    style: Add basic design token from figma spec

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,23 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      xs: '480px', // Custom breakpoint from Figma spec
+      // sm: '640px', // Disable built-in unused breakpoint
+      // md: '768px', // Disable built-in unused breakpoint
+      lg: '1024px', // Built-in breakpoint
+      // xl: '1280px', // Disable built-in unused breakpoint
+      // '2xl': '1536px', // Disable built-in unused breakpoint
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        primary: '#310C03',
+        secondary: '#F0E6DA',
       },
     },
   },
