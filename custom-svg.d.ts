a9a6524d369f91ba9c50b8f6ee73d20ed474a5f4
Eason Chang <eason@easonchang.com>
2024-05-08 21:38:26 -0600

    feat: Install svgr to support import svg as component

--- custom-svg.d.ts
+++
@@ -1,5 +0,0 @@
-// Support importing svg files in Next.js <Image /> component without TypeScript error
-declare module '*.svg' {
-  const content: React.FC<React.SVGProps<SVGSVGElement>>;
-  export default content;
-}
--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,14 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  // Support SVGs in Next.js
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
